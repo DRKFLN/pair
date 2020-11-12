b47b13d8a06062219c8099d3881098f08ef5281f
ilya <tabletkovv@mail.ru>
2020-11-13 00:16:19 +0300

    hello world

--- pair/pair/Program.cs
+++ pair/pair/Program.cs
@@ -7,6 +7,12 @@ namespace pair
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
